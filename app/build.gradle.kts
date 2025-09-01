958b9773b38f31079f08acfde3c5b42a6bb853c6
BelalSalem <62998842+TheBombh2@users.noreply.github.com>
2025-09-01 15:42:32 +0300

    updated gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.iti.graduation.recipeapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.iti.graduation.recipeapp"
