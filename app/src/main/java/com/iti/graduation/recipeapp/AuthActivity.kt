21689f6ff9be7c21e092be797624ca5062b2cd27
BelalSalem <62998842+TheBombh2@users.noreply.github.com>
2025-09-06 11:55:21 +0300

    Move AuthActivity

--- app/src/main/java/com/iti/graduation/recipeapp/AuthActivity.kt
+++ app/src/main/java/com/iti/graduation/recipeapp/AuthActivity.kt
@@ -1,11 +1,10 @@
-package com.iti.graduation.recipeapp.ui.auth
+package com.iti.graduation.recipeapp
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.iti.graduation.recipeapp.R
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
